e52381c103f22ab9564ee6c767370c1918b466ea
giito <giito0101@gmail.com>
2020-02-17 00:20:45 +0900

    ブログチュートリアル - レイヤーの追加:投稿記事の編集(実装完了) シンプルな認証と承認のアプリケーション (実装確認途中)

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -4,14 +4,29 @@ class PostsController extends AppController {
     public $helpers = array('Html', 'Form');
     public $layout = "Default";
 
+    public function isAuthorized($user) {
+        //登録済みユーザーは投稿できる
+        if($this->action === 'add') {
+            return true;
+        }
+
+        //投稿のオーナーは編集や削除ができる
+        if(in_array($this->action, array('edit', 'delete'))) {
+            $postId = (int) $this->request->params['pass'][0];
+            if ($this->Post->isOwnedBy($postId, $user['id'])) {
+                return true;
+            }
+        }
+
+        return parent::isAuthorized($user);
+    }
+
     public function index() {
         $this->set('posts', $this->Post->find('all'));
     }
 
     public function view($id = null) {
-        if(!$id) {  
-            throw new NotFoundException(__('Invalid post'));
-        }
+
 
         $post = $this->Post->findById($id);
         if (!$post) {
@@ -22,16 +37,49 @@ class PostsController extends AppController {
 
     public function add() {
         if ($this->request->is('post')) {
-            $this->Post->create();
+            //Added this line
+            $this->request->data['Post']['user_id'] = $this->Auth->user('id');
             if($this->Post->save($this->request->data)) {
-                $this->Flsh->success(__('Your post has been saved.'));
+                $this->Flash->success(__('Your post has been saved.'));
                 return $this->redirect(array('action' => 'index'));
             }
-            $this->Flash->error(__('Unable to add your post.'));
         }
     }
     
-    public function foobar() {
-        
+    public function edit($id = null) {
+        if(!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        $post = $this->Post->findById($id);
+        if(!$post) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        if($this->request->is(array('post', 'put'))) {
+            $this->Post->id = $id;
+            if($this->Post->save($this->request->data)) {
+                $this->Flash->success(__('Your post has been updated.'));
+                return $this->redirect(array('action' => 'index'));
+            }
+            $this->Flash->error(__('unable to update your post'));
+        }
+
+        if(!$this->request->data) {
+            $this->request->data = $post;
+        }
+    }
+
+    public function delete($id) {
+        if($this->request->is('get')) {
+            throw new MethodNotAllowedException();
+        }
+        if($this->Post->delete($id)) {
+            $this->Flash->success(
+                __('The post with id: %s has been deleted.', h($id))
+            );
+        }
+
+        return $this->redirect(array('action' => 'index'));
     }
 }
--- app/View/Posts/index.ctp
+++ app/View/Posts/index.ctp
@@ -1,14 +1,15 @@
 <!-- File: /app/View/Posts/index.ctp -->
 
 <h1>Blog posts</h1>
-<?php echo $this->Html->link(
+<p><?php echo $this->Html->link(
     'Add Post',
     array('controller' => 'posts', 'action' => 'add'));
-?>
+?></p>
 <table>
     <tr>
         <th>Id</th>
         <th>Title</th>
+        <th>Actions</th>
         <th>Created</th>
     </tr>
 
@@ -18,10 +19,18 @@
     <tr>
         <td><?php echo $post['Post']['id']; ?></td>
         <td>
+            <?php echo $this->Html->link($post['Post']['title'],
+             array('action' => 'view', $post['Post']['id'])); ?>
+        </td>
+        <td>
+            <?php echo $this->Form->postLink('Delete',
+             array('action' => 'delete', $post['Post']['id']),
+             array('confirm' => 'Are you sure?')); ?>
+        </td>
+        <td>
             <?php echo $this->Html->link($post['Post']['created'],
              array('controller' => 'posts', 'action' => 'view', $post['Post']['id'])); ?>
         </td>
-        <td><?php echo $post['Post']['created']; ?></td>
     </tr>
     <?php endforeach; ?>
     <?php unset($post); ?>
